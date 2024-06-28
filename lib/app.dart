4366ea1ac2b1d68513478e9f479c0bd65e95dd49
VivekMakwana7 <90848351+VivekMakwana7@users.noreply.github.com>
2024-06-28 20:33:04 +0530

    - Code Initial

---
+++ lib/app.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';import 'package:flutter_basics/src/home/page/home_page.dart';class MyApp extends StatelessWidget {  const MyApp({super.key});  @override  Widget build(BuildContext context) {    return const MaterialApp(      title: 'Flutter basics & Learning',      home: HomePage(),    );  }}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
